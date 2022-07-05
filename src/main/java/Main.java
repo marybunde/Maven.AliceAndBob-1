b4f11e72b5bba4197c83a0ef014ff8b123584133
marybunde <marybunde@gmail.com>
2022-07-04 23:12:59 -0400

    Finished Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,17 @@
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
+        //write program where user inputs his/her name.
+        //program should greet users whose names are 'Alice' and 'Bob'.
+    String name ="";
+    if ((name == "Alice") || (name == "Bob")) {
+         System.out.println("Hi " + name);
+    } else
+        System.out.println("Bye " + name);
 
     }
+
 }
+
+
